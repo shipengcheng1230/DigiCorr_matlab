df032cafc0c8dc6f05b457dadf0db8f0d1575d7e
Pengcheng Shi <Pengcheng Shi>
2015-12-20 20:41:38 +0800

    The building of my gui

---
+++ guitest.m
@@ -0,0 +1,16 @@
+contextObj = Context.getInstance();
+
+currentdata = CurrentData();
+signaldata = SignalData();
+pipinfo = PipInfo();
+processdata = ProcessData(currentdata, signaldata, pipinfo);
+
+contextObj.register('SignalData', signaldata);
+contextObj.register('ProcessData', processdata);
+contextObj.register('PipInfo', pipinfo);
+contextObj.register('CurrentData', currentdata);
+
+myfilter = FilterModel();
+contextObj.register('Filter', myfilter);
+
+manageview = ManageView();
